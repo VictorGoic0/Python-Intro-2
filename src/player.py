2fd2f48a4c1ba4edf82287e986963e6b01297d51
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-30 19:05:06 -0400

    Cleaning up more repetition.

--- src/player.py
+++ src/player.py
@@ -7,28 +7,30 @@ class Player:
     def getCurrentRoom(self):
         print(f'\n***** Current Room: {self.current_room.name} *****\n')
         print(f'\n***** Current Description: {self.current_room.description} *****\n')
+    def noRooms(self):
+        print("There are no more rooms in that directions. Try a different direction.")
     def travel(self, direction):
         if (direction == 'n'):
             if (self.current_room.n_to):
                 self.current_room = self.current_room.n_to
                 self.getCurrentRoom()
             else:
-                print("There are no more rooms in that directions. Try a different direction.")
+                self.noRooms()
         elif (direction == 's'):
             if (self.current_room.s_to):
                 self.current_room = self.current_room.s_to
                 self.getCurrentRoom()
             else:
-                print("There are no more rooms in that directions. Try a different direction.")
+                self.noRooms()
         elif (direction == 'e'):
             if (self.current_room.e_to):
                 self.current_room = self.current_room.e_to
                 self.getCurrentRoom()
             else:
-                print("There are no more rooms in that directions. Try a different direction.")
+                self.noRooms()
         elif (direction == 'w'):
             if (self.current_room.w_to):
                 self.current_room = self.current_room.w_to
                 self.getCurrentRoom()
             else:
-                print("There are no more rooms in that directions. Try a different direction.")
+                self.noRooms()
